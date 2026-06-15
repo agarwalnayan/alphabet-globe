a458b11c4069cafa0cd3e1233978e9bef2f16a91
Nayan <agarwalnayan480@gmail.com>
2026-06-16 00:08:54 +0530

    Frontend api check changed

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ export default function App() {
 
   const fetchModels = async () => {
   try {
-    const API_URL = import.meta.env.VITE_API_URL;
+    const API_URL = process.env.REACT_APP_API_URL;
 
     console.log("API_URL =", API_URL);
 
